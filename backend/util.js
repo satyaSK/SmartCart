ffb16972d19fb828be1d8b9e043f84819056355a
Joel Varghese George <joelasjo@gmail.com>
2019-11-18 15:56:47 -0800

    Add files via upload

--- backend/util.js
+++ backend/util.js
@@ -3,13 +3,39 @@ var MongoClient = require('mongodb').MongoClient;
 var uri = 'mongodb+srv://joel:11223345@db-32rt2.mongodb.net/test?retryWrites=true&w=majority';
 
 const functions = {
-    bdcon(){
+    dbcon(dbname, dbcol){
 
         MongoClient.connect(uri,{ useUnifiedTopology: true }, function(err, client) {
-            const collection = client.db("smartkart").collection("cart");
+            const collection = client.db(dbname).collection(dbcol);
             client.close();
         });
         return "connected";
+    },
+    dbitem(itmurl,itmname,itemprice){
+
+        var item = {
+          url: itmurl,
+          name: itmname,
+          price: itemprice
+        };
+        return item.url;
+
+
+    },
+    dbinsert(dbname, dbcol,itmurl,itmname,itemprice){
+        var item = {
+            url: itmurl,
+            name: itmname,
+            price: itemprice
+        };
+        MongoClient.connect(uri,{ useUnifiedTopology: true }, function(err, client) {
+          const collection = client.db(dbname).collection(dbcol);
+          assert.equal(null, err);
+          collection.insertOne(item,function(err,res){
+          });
+          client.close();
+        });
+        return "data inserted!!!"
     }
 }
 
--- backend/util.test.js
+++ backend/util.test.js
@@ -1,4 +1,10 @@
 const functions = require('./util');
 test('should output if db is connected', () =>{
-    expect(functions.bdcon()).toBe('connected');
+    expect(functions.dbcon('smartkart','test')).toBe('connected');
+});
+test('should output if item got in format', () =>{
+    expect(functions.dbitem('www','eeee','23')).toBe('www');
+});
+test('insert item to db', () =>{
+    expect(functions.dbinsert('smartkart', 'test2', 'www22', 'qwerty', '12')).toBe('data inserted!!!');
 });
