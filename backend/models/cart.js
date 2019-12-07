43b2ecdcfffd5a7ee890db9f352a9208eb92e96a
Joel Varghese George <joelasjo@gmail.com>
2019-12-06 16:00:18 -0800

    Add files via upload

--- backend/models/cart.js
+++ backend/models/cart.js
@@ -5,23 +5,10 @@ module.exports = function cart(oldCart) {
     this.totalPrice1 = oldCart.totalPrice1|| 0;
     this.totalfPrice = oldCart.totalfPrice|| 0;
     this.totalwPrice = oldCart.totalwPrice|| 0;
-    this.market = oldCart.market|| false;
-
+    this.market = oldCart.market|| null;
+    var fp,wp;
     this.add = function(item, id){
-        var storedItem = this.items[id];
-        if(!storedItem) {
-            storedItem = this.items[id] = {item: item,qty: 0, fprice: 0, wprice: 0};
-        }
-        storedItem.qty++;
-        storedItem.fprice = storedItem.item.fprice * storedItem.qty;
-        storedItem.wprice = storedItem.item.wprice * storedItem.qty;
-        this.totalQty++;
-        console.log(this.totalwPrice);
-        this.totalwPrice += storedItem.wprice;
-        console.log(this.totalwPrice);
-        console.log(this.totalfPrice);
-        this.totalfPrice += storedItem.fprice;
-        console.log(this.totalfPrice);
+        
         if(this.totalfPrice > this.totalwPrice){
             this.market= false;
             this.totalPrice = this.totalwPrice;
@@ -38,12 +25,18 @@ module.exports = function cart(oldCart) {
         this.items[id].fprice -= this.items[id].item.fprice;
         this.items[id].wprice -= this.items[id].item.wprice;
         this.totalQty--;
-        if(this.items[id].item.fprice > this.items[id].item.wprice)
-            this.totalPrice -= this.items[id].item.wprice;
-        else
-            this.totalPrice -= this.items[id].item.fprice;
         this.totalwPrice -= this.items[id].item.wprice;
         this.totalfPrice -= this.items[id].item.fprice;
+        if(this.totalfPrice > this.totalwPrice){
+            this.market= false;
+            this.totalPrice = this.totalwPrice;
+        }
+        else{
+            this.market= true;
+            this.totalPrice = this.totalfPrice;
+            console.log("else");
+        }
+
         if(this.items[id].qty <= 0){
             delete this.items[id];
         }
@@ -54,12 +47,17 @@ module.exports = function cart(oldCart) {
         this.items[id].fprice += this.items[id].item.fprice;
         this.items[id].wprice += this.items[id].item.wprice;
         this.totalQty++;
-        if(this.items[id].item.fprice > this.items[id].item.wprice)
-            this.totalPrice += this.items[id].item.wprice;
-        else
-            this.totalPrice += this.items[id].item.fprice;
         this.totalwPrice += this.items[id].item.wprice;
         this.totalfPrice += this.items[id].item.fprice;
+        if(this.totalfPrice > this.totalwPrice){
+            this.market= false;
+            this.totalPrice = this.totalwPrice;
+        }
+        else{
+            this.market= true;
+            this.totalPrice = this.totalfPrice;
+            console.log("else");
+        }
     };
 
     this.generateArray = function () {
